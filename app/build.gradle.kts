28bc24323026aab28f2ab3966b4aad459657e099
hakee <jshagold@gmail.com>
2025-05-28 14:48:50 +0900

    chore: 단어 추가, 조회, 삭제 기능 구현된 시작 버전 1.0.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,8 +16,8 @@ android {
         applicationId = "com.example.memorizingwords"
         minSdk = 28
         targetSdk = 35
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "1.0.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
