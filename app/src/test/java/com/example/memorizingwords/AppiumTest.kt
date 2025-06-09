235e3b23c7ade5125f654e27f499370cc28e56c9
hakee <jshagold@gmail.com>
2025-06-09 15:59:16 +0900

    feat: Appium 테스트 케이스

--- app/src/test/java/com/example/memorizingwords/AppiumTest.kt
+++ app/src/test/java/com/example/memorizingwords/AppiumTest.kt
@@ -210,4 +210,58 @@ class AppiumTest {
         button.click()
     }
 
+    @Test
+    fun test_7_Input_ID_PW() {
+        Thread.sleep(3000)
+        println("testFindBackButton: ${driver.pageSource}", )
+
+        // 2. 컨텍스트 목록 출력
+        val contexts = driver.contextHandles
+        println("Available contexts: $contexts")
+
+        // 3. WebView 컨텍스트 찾기
+        val webviewContext = contexts.find { it.contains("WEBVIEW") }
+            ?: throw RuntimeException("No WEBVIEW context found")
+
+        println("webviewContext: $webviewContext")
+
+        // 4. 컨텍스트 전환
+        driver.context(webviewContext)
+        println("Switched to context: $webviewContext")
+
+        // 5. WebView 내부 요소 상호작용
+        val inputElementID = driver.findElement(By.xpath("//input[@placeholder='아이디를 입력해주세요.']") )
+        inputElementID.sendKeys("boss5821")
+
+        val inputElementPW = driver.findElement(By.xpath("//input[@placeholder='영문,숫자,특수문자 조합 8자리 이상']") )
+        inputElementPW.sendKeys("moa5821!")
+    }
+
+    @Test
+    fun test_8_Click_Login_Button() {
+        Thread.sleep(3000)
+        println("testFindBackButton: ${driver.pageSource}", )
+
+        // 2. 컨텍스트 목록 출력
+        val contexts = driver.contextHandles
+        println("Available contexts: $contexts")
+
+        // 3. WebView 컨텍스트 찾기
+        val webviewContext = contexts.find { it.contains("WEBVIEW") }
+            ?: throw RuntimeException("No WEBVIEW context found")
+
+        println("webviewContext: $webviewContext")
+
+        // 4. 컨텍스트 전환
+        driver.context(webviewContext)
+        println("Switched to context: $webviewContext")
+
+        // 5. WebView 내부 요소 상호작용
+        val loginBtn = driver.findElement(By.xpath("//div[@class='btn_full btn_login active']") )
+        loginBtn.click()
+
+    }
+
+
+
 }
