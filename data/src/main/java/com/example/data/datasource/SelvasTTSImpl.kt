af5f5610162b38b84e65d118f629bbb2165270d7
hakee <jshagold@gmail.com>
2025-06-23 13:28:51 +0900

    jenkins 연동

--- data/src/main/java/com/example/data/datasource/SelvasTTSImpl.kt
+++ data/src/main/java/com/example/data/datasource/SelvasTTSImpl.kt
@@ -15,7 +15,7 @@ import javax.inject.Inject
 
 class SelvasTTSImpl @Inject constructor() : SelvasTTS {
 
-    private val pttsNet = Pttsnet()
+//    private val pttsNet = Pttsnet()
 
     override fun downloadVoiceFile(baseDir: File, textList: List<String>): Flow<Result<Pair<Int, String>>> = flow {
         val files = baseDir.listFiles()
@@ -25,44 +25,47 @@ class SelvasTTSImpl @Inject constructor() : SelvasTTS {
             val returnValue = downVoiceFileToBuffer(text)
             Napier.e { "return value $returnValue" }
 
-            if(pttsNet.SpeechBuffer != null) {
-                pttsNet.SpeechBuffer?.let { byteArray ->
-                    val ttsFile = File(baseDir.path, "${index}.mp3")
-                    val tempFile = File(ttsFile.parentFile, "${ttsFile.name}.tmp")
 
-                    BufferedOutputStream(FileOutputStream(tempFile)).use { outputStream ->
-                        outputStream.write(byteArray)
-                    }
-                    tempFile.renameTo(ttsFile)
-
-                    emit(Result.Success(Pair(index, ttsFile.name)))
-                }
-            } else {
-                Napier.e { "pttsNet.SpeechBuffer is null ${pttsNet.errorMsg}" }
-                emit(Result.Failure(index.toString()))
-            }
+            emit(Result.Failure(index.toString()))
+//            if(pttsNet.SpeechBuffer != null) {
+//                pttsNet.SpeechBuffer?.let { byteArray ->
+//                    val ttsFile = File(baseDir.path, "${index}.mp3")
+//                    val tempFile = File(ttsFile.parentFile, "${ttsFile.name}.tmp")
+//
+//                    BufferedOutputStream(FileOutputStream(tempFile)).use { outputStream ->
+//                        outputStream.write(byteArray)
+//                    }
+//                    tempFile.renameTo(ttsFile)
+//
+//                    emit(Result.Success(Pair(index, ttsFile.name)))
+//                }
+//            } else {
+//                Napier.e { "pttsNet.SpeechBuffer is null ${pttsNet.errorMsg}" }
+//                emit(Result.Failure(index.toString()))
+//            }
         }
     }.flowOn(Dispatchers.IO)
 
     // return이 0 이상이면 성공
     private fun downVoiceFileToBuffer(text: String): Int {
         return try {
-            pttsNet.PTTSNET_BUFFER(
-                SelvasConfig.SERVICE_HOST,
-                SelvasConfig.SERVICE_PORT,
-                SelvasConfig.CONN_TIMEOUT,
-                SelvasConfig.READ_TIMEOUT,
-                text,
-                SelvasConfig.LANGUAGE,
-                SelvasConfig.SPEAKER_2,
-                Pttsnet.PTTSNET_FMT22K_16BIT_WAVE,
-                SelvasConfig.PROP_PITCH,
-                SelvasConfig.PROP_SPEED,
-                SelvasConfig.PROP_VOLUME,
-                SelvasConfig.OPT_NFLAG,
-                SelvasConfig.OPT_CONTENT_TYPE,
-                SelvasConfig.OPT_CHARACTER_SET
-            )
+            0
+//            pttsNet.PTTSNET_BUFFER(
+//                SelvasConfig.SERVICE_HOST,
+//                SelvasConfig.SERVICE_PORT,
+//                SelvasConfig.CONN_TIMEOUT,
+//                SelvasConfig.READ_TIMEOUT,
+//                text,
+//                SelvasConfig.LANGUAGE,
+//                SelvasConfig.SPEAKER_2,
+//                Pttsnet.PTTSNET_FMT22K_16BIT_WAVE,
+//                SelvasConfig.PROP_PITCH,
+//                SelvasConfig.PROP_SPEED,
+//                SelvasConfig.PROP_VOLUME,
+//                SelvasConfig.OPT_NFLAG,
+//                SelvasConfig.OPT_CONTENT_TYPE,
+//                SelvasConfig.OPT_CHARACTER_SET
+//            )
         } catch (e: Exception) {
             Napier.e(throwable = e, message = "pttsNet Error")
             0
